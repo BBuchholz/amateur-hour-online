8aedcc105a4129b1711145586891057c3f395654
ENTOMDT <entomdt@gmail.com>
2022-12-31 11:29:04 -0600

    Add Cypress spec examples and start tutorial spec

--- cypress/component/NavBarMain.cy.js
+++
@@ -1,5 +0,0 @@
-describe('NavBarMain.cy.js', () => {
-  it('playground', () => {
-    // cy.mount()
-  })
-})
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+describe('My First Test', () => {
+
+  it('Does not need to do much atm!', () => {
+  
+    expect(true).to.equal(true);
+
+  })
+
+});
